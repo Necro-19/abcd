3ab469268eb0f94122bddc825dc18fab83d0b93e
NecroMancer <145217219+Necro-19@users.noreply.github.com>
2024-05-02 07:38:27 +0530

    Delete WAD/3_a/index.js

--- WAD/3_a/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("public"));
-
-app.listen(4000, () => {
-  console.log("app is listening on http://localhost:4000");
-});
